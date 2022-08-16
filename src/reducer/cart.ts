9508b6abb3eea478c6a7b3828a8750cdc9a546a2
shanky_ms <shankaranarayanan.m.s@gmail.com>
2022-08-16 10:37:26 +0530

    Added success and product quantity updates

--- src/reducer/cart.ts
+++ src/reducer/cart.ts
@@ -10,33 +10,56 @@ const initialState: CartInitState = {
 	products: [],
 };
 
+const getProductIndexFromCart = (products: Array<CartProduct>, payload: ProductCartType): number => {
+	const { product, variant } = payload;
+	const productIndex = products.findIndex((stateProduct) => {
+		const isSameProduct = stateProduct.product.id === product.id;
+		const isSameVariant = stateProduct.variant.id === variant.id;
+		return isSameProduct && isSameVariant;
+	});
+	return productIndex;
+};
+
 export const cartSlice = createSlice({
 	name: "cart",
 	initialState,
 	reducers: {
 		addProductToCart: (state, { payload }: PayloadAction<ProductCartType>) => {
-			const { product, variant } = payload;
-			const existingIndex = state.products.findIndex((stateProduct) => {
-				const isSameProduct = stateProduct.product.id === product.id;
-				const isSameVariant = stateProduct.variant.id === variant.id;
-				return isSameProduct && isSameVariant;
-			});
+			const existingIndex = getProductIndexFromCart(state.products, payload);
 			// If the product already exists then increase the quantity by 1
 			if (existingIndex >= 0) {
 				state.products[existingIndex].quantity = state.products[existingIndex].quantity + 1;
 				return;
 			}
-
 			state.products.push({ ...payload, quantity: 1 });
 		},
-		removeProduct: () => {},
-		alterProduct: () => {},
+		increaseQuantity: (state, { payload }: PayloadAction<ProductCartType>) => {
+			const existingIndex = getProductIndexFromCart(state.products, payload);
+			if (existingIndex >= 0) {
+				state.products[existingIndex].quantity = state.products[existingIndex].quantity + 1;
+				return;
+			}
+		},
+		decreaseQuantity: (state, { payload }: PayloadAction<ProductCartType>) => {
+			const existingIndex = getProductIndexFromCart(state.products, payload);
+			const currentQuantity = state.products[existingIndex].quantity;
+			if (existingIndex >= 0 && currentQuantity > 1) {
+				state.products[existingIndex].quantity = state.products[existingIndex].quantity - 1;
+				return;
+			} else {
+				console.log("Throw a warning if it's less than 0");
+			}
+		},
+		clearCart: (state) => {
+			state = initialState;
+			return state;
+		},
 	},
 });
 
 const { actions, reducer } = cartSlice;
 
-export const { addProductToCart, removeProduct, alterProduct } = actions;
+export const { addProductToCart, clearCart, increaseQuantity, decreaseQuantity } = actions;
 
 const selectSelf = (state: RootState) => state.cart;
 
