a498bdb5cc592f67a7d279a11e544ec2743473da
msShanky <shankaranarayanan.m.s@gmail.com>
2023-04-14 12:58:19 +0530

    social media links and shipping delivery policy

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,6 +2,9 @@ import React, { FunctionComponent, ReactElement } from "react";
 import Head from "next/head";
 import AppHeader from "../common/AppHeader";
 import AppFooter from "../common/AppFooter";
+import Link from "next/link";
+
+import { IconBrandFacebook, IconBrandWhatsapp } from "@tabler/icons-react";
 
 type AppLayoutProps = {
 	children: ReactElement;
@@ -21,6 +24,26 @@ const AppLayout: FunctionComponent<AppLayoutProps> = (props) => {
 			</Head>
 			<main className={isLanding ? "mb-32" : "my-32"}>{children}</main>
 			<AppFooter menuLinks={menuLinks} />
+			<div className="fixed flex flex-col gap-4 bottom-10 md:right-10 right-2">
+				<Link passHref legacyBehavior href="https://wa.me/message/ZDVSMN5VC3SCO1">
+					<a target="_blank" rel="noreferrer" href="https://wa.me/message/ZDVSMN5VC3SCO1">
+						<div className="flex items-center justify-center w-10 h-10 rounded-full md:w-12 md:h-12 bg-secondary">
+							<IconBrandWhatsapp className=" stroke-primaryBlack" size={30} />
+						</div>
+					</a>
+				</Link>
+				<Link passHref legacyBehavior href="https://www.facebook.com/profile.php?id=100083119281213&mibextid=ZbWKwL">
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href="https://www.facebook.com/profile.php?id=100083119281213&mibextid=ZbWKwL"
+					>
+						<div className="flex items-center justify-center w-10 h-10 rounded-full md:w-12 md:h-12 bg-secondary">
+							<IconBrandFacebook className="fill-primaryBlack" />
+						</div>
+					</a>
+				</Link>
+			</div>
 		</>
 	);
 };
